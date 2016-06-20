e816879ee2e7562ed5eb8477a988f48abecb5be9
sujeeth19093 <sujeeth19093@gmail.com>
2016-06-20 13:53:15 -0700

    Update test.cpp

--- firmware/examples/test.cpp
+++ firmware/examples/test.cpp
@@ -1,1 +1,27 @@
-//
+#include "Particle.h"
+
+#include "EEPROM_ACCESS_PARTICLE/EEPROM_ACCESS_PARTICLE.h"
+
+String msg;
+int i;
+
+void setup()
+{
+	i=0;
+}
+
+void loop()
+{
+	sprintf(msg, "%d - This should keep repeating.", i);
+	i++;
+	boolean sent = Particle.publish("access_test", msg, PRIVATE);
+	
+	delay(1000);
+	
+	processData();
+	
+	if(!sent)
+	{
+		storeData(msg);
+	}
+}
