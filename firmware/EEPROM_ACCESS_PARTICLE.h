ab5b84bb7b12b3b22f876fe5b9ff45c2664183f1
sujeeth19093 <sujeeth19093@gmail.com>
2016-06-21 10:39:52 -0700

    Update EEPROM_ACCESS_PARTICLE.h

--- firmware/EEPROM_ACCESS_PARTICLE.h
+++ firmware/EEPROM_ACCESS_PARTICLE.h
@@ -1,15 +1,12 @@
-#ifndef _EEPROM_ACCESS
-#define _EEPROM_ACCESS
+#ifndef _EEPROM_ACCESS_PARTICLE
+#define _EEPROM_ACCESS_PARTICLE
 
 #include "application.h"
 
 #define DATA_IN_EEP 1
 #define NO_DATA_IN_EEP 0
-#define STRING_LEN 30
 
-//void addToQueue(String msg);
-void storeData(String msg);
-//void processQueue();
+void storeData(String msg, int len);
 void processData();
 
 #endif
