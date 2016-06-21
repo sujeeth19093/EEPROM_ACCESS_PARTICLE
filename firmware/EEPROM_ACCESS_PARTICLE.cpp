b9aad20ae9331bf326f6f6dcdb75edddb768de2b
sujeeth19093 <sujeeth19093@gmail.com>
2016-06-21 10:39:26 -0700

    Update EEPROM_ACCESS_PARTICLE.cpp

--- firmware/EEPROM_ACCESS_PARTICLE.cpp
+++ firmware/EEPROM_ACCESS_PARTICLE.cpp
@@ -1,101 +1,61 @@
 #include "EEPROM_ACCESS_PARTICLE.h"
 
-//typedef struct reading {
-//    String data;					//Assuming a maximum of 30 characters
-//    struct reading * next;
-//} reading_t;
-
-//reading_t * reading_queue_head = NULL;
-
-void storeData(String msg)
+void storeData(String msg, int len)
 {
 	int position;
 	int offset;
-	
-	EEPROM.put(0, DATA_IN_EEP);		//Indicates that data is present in EEPROM
+	String temp;
+	int status;
+
+	EEPROM.get(0, status);
+	if(status != DATA_IN_EEP)
+	{
+		EEPROM.put(0, DATA_IN_EEP);		//Indicates that data is present in EEPROM
+		EEPROM.put(5, 0);
+	}
+
 	EEPROM.get(5, position);		//The starting position of most recently stored data
-	
+
 	if(position == 0)				//Sets the start point of the new data to be stored
 	{
 		offset = position + 10;
 	}else
 	{
-		offset = position + STRING_LEN;
+		EEPROM.get(position, temp);
+		offset = position + temp.length() + 1;
 	}
-	
+
 	EEPROM.put(offset, msg);
-	
+	EEPROM.put(offset + len, '\0');
+
 	EEPROM.put(5, offset);			//Updates the start position
 }
-
-/*void addToQueue(String msg)
-{
-  reading_t * new_reading = new reading_t;
-  new_reading->next = reading_queue_head;
-  reading_queue_head = new_reading;//store the value
-  new_reading->data = msg;
-}
-
-void processQueue()
-{
-	reading_t * current = reading_queue_head;
-  
-  while(reading_queue_head != NULL){
-    current = reading_queue_head;
-    boolean success = Particle.publish("colorinfo", reading_queue_head->data);	//I assume that colorinfo is used everywhere
-
-    if(success){					//message was sent
-      reading_queue_head = reading_queue_head->next;
-      free(current);
-      if(reading_queue_head != NULL){
-        delay(1000); 				//wait a second before trying the next message Particle does not let us go faster than this
-      }else{						//we are done here
-        break;
-      }
-    }else{							//we failed to send try again later
-		break;
-    }
-  }
-}*/
 
 void processData()
 {
 	int status;
-	EEPROM.get(0,status);
-	
+	EEPROM.get(0, status);
+
 	if(status == DATA_IN_EEP)
 	{
-		int position;
-		char msg[32];
+		int position = 10;
+		int limit;
+		String msg;
 		boolean fail;
-		
-		EEPROM.get(5,position);			//Gets position of latestest data stored	
-		
-		while(position > 10)
+
+		EEPROM.get(5, limit);			//Gets position of latestest data stored
+
+		while(limit >= position)
 		{
-			for(int j = 0; j < 32; j++)
-			{
-				EEPROM.get(position + j, msg[j]);	//copies data
-			}
-			boolean success = Particle.publish("colorinfo", msg);	//publishes data onto the cloud
-			if(!success)				//data to cloud transfer is unsuccesful
-			{
-				EEPROM.put(5, position);	//the position of latest data changes 
-				fail = TRUE;
-				break;				
-			}
-			//addToQueue(msg);			//adds data onto Q.
-			position = position - STRING_LEN;	//position is shifted to next data stored
+			EEPROM.get(position, msg);	//copies data
+			boolean success = Particle.publish("eeprom data", msg);	//publishes data onto the cloud
+			position = position + msg.length() + 1;	//position is shifted to next data stored
 		}
-		
-		if(!fail)
-		{
+
 			position = 0;
-			
+
 			EEPROM.clear();					//clears all data in  EEPROM
 			EEPROM.put(5, position);		//sets initial position
 			EEPROM.put(0, NO_DATA_IN_EEP);		//indicates no data in EEPROM
-		}
-		//processQueue();					//processes data in correct order of stroage
 	}
 }
