37320e26e0e8648fe72b091ace232576bbc200b2
sujeeth19093 <sujeeth19093@gmail.com>
2016-06-27 11:43:10 -0700

    Update EEPROM_ACCESS_PARTICLE.cpp

--- firmware/EEPROM_ACCESS_PARTICLE.cpp
+++ firmware/EEPROM_ACCESS_PARTICLE.cpp
@@ -1,63 +1,56 @@
 #include "EEPROM_ACCESS_PARTICLE.h"
 
-void storeData(String msg, int len)
+int status_store;
+int position_store;
+void storeData()
 {
-	int position;
-	int offset;
-	String temp;
-	int status;
+  EEPROM.get(0, status_store);
+  if(status_store == NO_DATA_IN_EEP)
+  {
+    EEPROM.put(0, DATA_IN_EEP);
+    EEPROM.put(5, 0);
+  }
 
-	EEPROM.get(0, status);
-	if(status != DATA_IN_EEP)
-	{
-		EEPROM.put(0, DATA_IN_EEP);		//Indicates that data is present in EEPROM
-		EEPROM.put(5, 0);
-	}
-
-	EEPROM.get(5, position);		//The starting position of most recently stored data
-
-	if(position == 0)				//Sets the start point of the new data to be stored
-	{
-		offset = position + 10;
-	}else
-	{
-		EEPROM.get(position, temp);
-		offset = position + temp.length() + 1;
-	}
-
-	EEPROM.put(offset, msg);
-	Serial.printf("%s\n", msg.c_str());//////////////////////////////////////////////////////////////
-	EEPROM.put(offset + len, '\0');
-
-	EEPROM.put(5, offset);			//Updates the start position
+  EEPROM.get(5, position_store);
+  if(position_store == 0)
+  {
+    position_store = 10;
+  }else
+  {
+    position_store = position_store + 35;
+  }
+    EEPROM.put(position_store,store_str);
+    delay(1000);
+  //  Particle.publish("e-test-store", s1);
+    Serial.printf("position_store = %d\r\n", position_store);
+    EEPROM.put(5, position_store);
 }
 
+String s2;
+int status_process;
+int limit_process, position_process;
 void processData()
 {
-	int status;
-	EEPROM.get(0, status);
-
-	if(status == DATA_IN_EEP)
-	{
-		int position = 10;
-		int limit;
-		String msg;
-		boolean success;
-
-		EEPROM.get(5, limit);			//Gets position of latest data stored
-
-		while(limit >= position)
-		{
-			EEPROM.get(position, msg);	//copies data
-			Serial.printf("%s\n", msg.c_str());//////////////////////////////////////////////////////////
-			success = Particle.publish("eeprom data", msg);	//publishes data onto the cloud
-			position = position + msg.length() + 1;	//position is shifted to next data stored
-		}
-
-			position = 0;
-
-			EEPROM.clear();					//clears all data in  EEPROM
-			EEPROM.put(5, position);		//sets initial position
-			EEPROM.put(0, NO_DATA_IN_EEP);		//indicates no data in EEPROM
-	}
+  EEPROM.get(0, status_process);
+  Serial.printf("status_process = %d\r\n", status_process);
+  if(status_process == DATA_IN_EEP)
+  {
+    limit_process = 0;
+    position_process = 10;
+    EEPROM.get(5, limit_process);
+    Serial.printf("limit_process = %d\r\n", limit_process);
+    while(limit_process >= position_process)
+    {
+      Particle.process();
+      Serial.printf("position_process = %d\r\n", position_process);
+      EEPROM.get(position_process, s2);
+      delay(1000);
+      Particle.publish("e-test", s2);
+      Serial.printf("Published\r\n");
+      position_process = position_process + 35;
+    }
+    EEPROM.clear();
+    EEPROM.put(0, NO_DATA_IN_EEP);
+    EEPROM.put(5, 0);
+  }
 }
